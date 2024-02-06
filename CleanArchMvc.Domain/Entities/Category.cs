e13d628444489ca1d0a0c64a19de10b524953577
André Messias Marestoni <m.marestoni@gmail.com>
2024-02-06 15:24:19 -0300

    adicionando UnitTest e Dbcontext

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,29 @@ namespace CleanArchMvc.Domain.Entities
         public int Id { get; private set; }
         public string Name { get; private set; }
 
+        public Category(string name) {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name) {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Ivalid name, Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Ivalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
